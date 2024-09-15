07d61fbb784e7b8ff350e459872b7ed9cc1c70b6
AsimMasood97 <asim.masood707@gmail.com>
2024-09-15 17:36:01 +0500

    Completed About page cotent and styling

--- About.js
+++ About.js
@@ -1,5 +1,8 @@
 const menu = document.querySelector(".menuItems");
 const info = document.querySelector(".info");
+
+// Menu ka color change karna ha
+
 menu.addEventListener("click", (e) => {
 	for (const element of info.children) {
 		if (element.classList.contains(e.target.innerText)) {
